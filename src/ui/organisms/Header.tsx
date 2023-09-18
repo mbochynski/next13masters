efc8f3029a7308f7a18a2d1b4f129ce07bc0d682
Mateusz Bochynski <mbochynski@pgs-soft.com>
2023-09-18 17:06:32 +0200

    feat: add partial match to ActiveLink

--- src/ui/organisms/Header.tsx
+++ src/ui/organisms/Header.tsx
@@ -3,10 +3,14 @@ import { ActiveLink } from "../atoms/ActiveLink";
 export const Header = () => {
 	return (
 		<nav className="flex justify-center py-4">
-			<ActiveLink href="/" className="p-2 text-blue-600" activeClassName="underline">
+			<ActiveLink href="/" exact className="p-2 text-lg text-blue-600" activeClassName="underline">
 				Home
 			</ActiveLink>
-			<ActiveLink href="/products" className="p-2 text-blue-600" activeClassName="underline">
+			<ActiveLink
+				href="/products"
+				className="p-2 text-lg text-blue-600"
+				activeClassName="underline"
+			>
 				All
 			</ActiveLink>
 		</nav>
